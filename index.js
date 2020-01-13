bd05d57d2c9ed39cc86928e3dd96b91bacf3ac44
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-13 16:03:41 -0500

    Basic server set up

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+
+//GET to "/"
+server.get('/', (req,res) => {
+    res.send({hello: 'Your server is working!'})
+})
+
+const port = 8000;
+
+server.listen(port, () => console.log(`\n ** api on port: ${port}\n`));

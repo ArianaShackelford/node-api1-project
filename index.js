3edb83fe068c8f11a171213708054aa65adeb902
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-13 17:35:46 -0500

    finished crud operations added express-validator

--- index.js
+++ index.js
@@ -21,7 +21,60 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({errorMessage: "sorry, we ran into an error getting the list of users"})
+        res.status(500).json({errorMessage: "sorry, we ran into an error getting the list of users"});
+    });
+});
+
+//Get user by id
+server.get('/api/users/:id', (req,res)=>{
+    const id = req.params.id;
+    Users.findById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "sorry, we ran into and finding that user"})
+    })
+})
+
+//Create a new user
+server.post('/api/users', (req, res) => {
+    const userData = req.body; //need validation here I believe but I'm not sure how...
+    Users.insert(userData)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({errorMessage: "sorry, we ran into an error adding a user"});
+    });
+});
+
+//Delete a user
+server.delete('/api/users/:id', (req,res)=>{
+    const id = req.params.id;
+    // console.log(req);
+    Users.remove(id)
+    .then(deleted=> {
+        res.status(200).json(deleted);
+    })//returns the number of records deleted
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "sorry, we ran into and error deleting that user"})
+    })
+})
+
+//Edit user info 
+server.put('/api/users/:id', (req,res)=>{
+    const id = req.params.id;
+    Users.update(id, req.body)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "sorry, we ran into and changing that users info"})
     })
 })
 

569dd6ce967fb335de591178acaab7343a4515be
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-13 16:09:27 -0500

    used get method to display list of users

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // implement your API here
 const express = require('express');
 
+const Users = require('./data/db.js');
+
 const server = express();
 
 server.use(express.json());
@@ -9,6 +11,18 @@ server.use(express.json());
 //GET to "/"
 server.get('/', (req,res) => {
     res.send({hello: 'Your server is working!'})
+})
+
+//View the list of users
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "sorry, we ran into an error getting the list of users"})
+    })
 })
 
 const port = 8000;
